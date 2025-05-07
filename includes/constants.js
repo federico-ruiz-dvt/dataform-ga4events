aabe5b121bce0b47b619d7e58409189e1006f29c
Fede Ruiz <federico.ruiz@devoteam.com>
2025-05-07 19:38:32 +0200

    Update WORK_SCHEMA constants.js
    Usar dataform_sources para trabajo integrado

--- includes/constants.js
+++ includes/constants.js
@@ -1,5 +1,5 @@
 const GA4_DATABASE = 'bigquery-public-data';
 const GA4_DATASET = 'ga4_obfuscated_sample_ecommerce';
 const GA4_TABLE = 'events_20210131';
-const WORK_SCHEMA = 'dataform_federuiz_ws'
+const WORK_SCHEMA = 'dataform_sources'
 module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE, WORK_SCHEMA}
